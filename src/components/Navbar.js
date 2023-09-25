aa1b4c477fcbb7f547cf5e3ba030098be0454bf1
SumitMehto15 <sumitmehto1507@gmail.com>
2023-09-25 15:05:28 +0530

    minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,27 +7,27 @@ export default function Navbar(props) {
         <div>
             <nav className={`navbar navbar-expand-lg bg-${props.mode=== 'light'?'danger': 'dark'} text-${props.mode=== 'light'?'dark': 'light'}`}>
                 <div className="container-fluid">
-                    <a className={`navbar-brand my-1 mx-3 text-${props.mode=== 'light'?'dark': 'light'}`} href="/"><b>{props.tittle}</b></a>
+                    <a className={`navbar-brand my-1 mx-3 text-${props.mode=== 'light'?'dark': 'light'}`} href="#"><b>{props.tittle}</b></a>
                     <button className="navbar-toggler " type="button text-light" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon text-light"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className={`nav-link menu1 active mx-3 mx-3 text-${props.mode=== 'light'?'dark': 'light'}`} aria-current="page" href="/"><b className='menu1'>Home</b></a>
+                                <a className={`nav-link menu1 active mx-3 mx-3 text-${props.mode=== 'light'?'dark': 'light'}`} aria-current="page" href="#"><b className='menu1'>Home</b></a>
                             </li>
                             <li className="nav-item ">
-                                <a className={`nav-link menu1 mx-3 mx-3 text-${props.mode=== 'light'?'dark': 'light'}`} href="/about"><b className='menu1'>About Us</b></a>
+                                <a className={`nav-link menu1 mx-3 mx-3 text-${props.mode=== 'light'?'dark': 'light'}`} href="#"><b className='menu1'>About Us</b></a>
                             </li>
                             <li className="nav-item menu1 dropdown mx-3 mx-3">
-                                <a className={`nav-link dropdown-toggle text-${props.mode=== 'light'?'dark': 'light'}`} href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a className={`nav-link dropdown-toggle text-${props.mode=== 'light'?'dark': 'light'}`} href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                    <b className='menu1'>Dropdown</b> 
                                 </a>
                                 <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item menu1" href="/"><b>Puzzle Games</b></a></li>
-                                    <li><a className="dropdown-item menu1" href="/"> <b>Mind Hunters </b></a></li>
+                                    <li><a className="dropdown-item menu1" href="#"><b>Puzzle Games</b></a></li>
+                                    <li><a className="dropdown-item menu1" href="#"> <b>Mind Hunters </b></a></li>
                                     <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item menu1 " href="/"><b>Join Our Team</b></a></li>
+                                    <li><a className="dropdown-item menu1 " href="#"><b>Join Our Team</b></a></li>
                                 </ul>
                             </li>
 
